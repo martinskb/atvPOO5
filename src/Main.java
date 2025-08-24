ca0af43a707acbd77918c4e9a42670fc17956f5b
kaio <kaio22barra@gmail.com>
2025-08-24 19:02:21 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Carro carro1 = new Carro();
+        carro1.marca = "Ferrari";
+        carro1.modelo = "F8 Tributo";
+        carro1.ano = 2023;
+
+        Carro carro2 = new Carro();
+        carro2.marca = "Bugatti";
+        carro2.modelo = "Chiron";
+        carro2.ano = 2022;
+
+
+        System.out.println("--- Informações iniciais ---");
+        carro1.mostrarInformacoes();
+        carro2.mostrarInformacoes();
+
+
+        carro2.marca = "Porsche";
+        carro2.modelo = "911 Turbo S";
+        carro2.ano = 2024;
+
+
+        System.out.println("\n--- Valores modificados ---");
+        carro1.mostrarInformacoes();
+        carro2.mostrarInformacoes();
+
+
+        System.out.println("\n--- Ligando os carros ---");
+        carro1.ligar();
+        carro2.ligar();
     }
 }
