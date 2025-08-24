b9c4eb472b32b601c7b816d1da355ccde4dd24d0
kaio <kaio22barra@gmail.com>
2025-08-24 19:01:35 -0300

    Update Carro.java

--- src/Carro.java
+++ src/Carro.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Carro {
+    String marca;
+    String modelo;
+    int ano;
+
+    public void mostrarInformacoes() {
+        System.out.println("Marca: " + marca + " | Modelo: " + modelo + " | Ano: " + ano);
+    }
+
+    public void ligar() {
+        System.out.println(marca + " " + modelo + " está ligado! 🚗");
+    }
 }
